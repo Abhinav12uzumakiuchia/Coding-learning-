c85d263cfe331be0c12e7ab72d7fd9d7b49a1dfe
Abhinav12uzumakiuchia <rubiakanksha2024@gmail.com>
2025-03-10 19:15:55 +0530

    Add files via upload

---
+++ Spotify Clone/script.js
@@ -0,0 +1,13 @@
+console.log("Let's write Javascript")
+
+async function main() {
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as= div.getElementsByTagName("a")
+    console.log(as)
+}
+
+main()
